c10a829ff87d5575d644c72a22bd82512208435d
olaysco <olayiwolaodunsi@gmail.com>
2020-06-02 16:54:36 +0100

    Updates

---
+++ precache-manifest.a9d465b7583686f3c837be581ea4620f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a67edda6f10fe20b0d7d8206a29fe6",
+    "url": "/neo-toast/index.html"
+  },
+  {
+    "revision": "04873ec7e42730cced55",
+    "url": "/neo-toast/static/css/main.29a14cbf.chunk.css"
+  },
+  {
+    "revision": "a0b6856a20d34a304942",
+    "url": "/neo-toast/static/js/2.bb63f1f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/neo-toast/static/js/2.bb63f1f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04873ec7e42730cced55",
+    "url": "/neo-toast/static/js/main.d1d6ae06.chunk.js"
+  },
+  {
+    "revision": "092791b660b1a45a9fae",
+    "url": "/neo-toast/static/js/runtime-main.d01a1036.js"
+  }
+]);
